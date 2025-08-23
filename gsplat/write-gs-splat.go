887c587e25ad6b0c73833f7edd6e7e95d709c233
gotoeasy <gotoeasy@163.com>
2025-08-23 17:36:42 +0800

    PrintSplat

--- gsplat/write-gs-splat.go
+++ gsplat/write-gs-splat.go
@@ -2,6 +2,7 @@ package gsplat
 
 import (
 	"bufio"
+	"fmt"
 	"gsbox/cmn"
 	"os"
 )
@@ -24,6 +25,27 @@ func WriteSplat(splatFile string, rows []*SplatData) {
 		bts = append(bts, rows[i].ColorR, rows[i].ColorG, rows[i].ColorB, rows[i].ColorA)
 		bts = append(bts, rows[i].RotationW, rows[i].RotationX, rows[i].RotationY, rows[i].RotationZ)
 		_, err = writer.Write(bts)
+		cmn.ExitOnError(err)
+	}
+	err = writer.Flush()
+	cmn.ExitOnError(err)
+}
+
+func PrintSplat(splatFile string, rows []*SplatData) {
+	file, err := os.Create(splatFile)
+	cmn.ExitOnError(err)
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	for i := range rows {
+		line := fmt.Sprintf("%s,%s,%s, %s,%s,%s, %v,%v,%v,%v, %v,%v,%v,%v\r\n",
+			cmn.FormatFloat32(rows[i].PositionX), cmn.FormatFloat32(rows[i].PositionY), cmn.FormatFloat32(rows[i].PositionZ),
+			cmn.FormatFloat32(cmn.EncodeSplatScale(rows[i].ScaleX)), cmn.FormatFloat32(cmn.EncodeSplatScale(rows[i].ScaleY)), cmn.FormatFloat32(cmn.EncodeSplatScale(rows[i].ScaleZ)),
+			rows[i].ColorR, rows[i].ColorG, rows[i].ColorB, rows[i].ColorA,
+			rows[i].RotationW, rows[i].RotationX, rows[i].RotationY, rows[i].RotationZ,
+		)
+		_, err = writer.WriteString(line)
 		cmn.ExitOnError(err)
 	}
 	err = writer.Flush()
