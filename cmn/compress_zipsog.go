e9926a5541e805ba3ff2fb63862bcf5dbd4b91cf
gotoeasy <gotoeasy@163.com>
2025-11-08 14:20:22 +0800

    sog

---
+++ cmn/compress_zipsog.go
@@ -0,0 +1,45 @@
+package cmn
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// 打包成一个sog文件
+func ZipSogFiles(zipAsSogFile string, files []string) {
+	os.MkdirAll(filepath.Dir(zipAsSogFile), 0666)
+
+	zipFile, err := os.Create(zipAsSogFile)
+	ExitOnError(err)
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		addFileToZip(zipWriter, file)
+	}
+}
+
+func addFileToZip(zipWriter *zip.Writer, filePath string) {
+	file, err := os.Open(filePath)
+	ExitOnError(err)
+	defer file.Close()
+
+	info, err := file.Stat()
+	ExitOnError(err)
+
+	// 只保留文件名，去掉目录路径
+	header, err := zip.FileInfoHeader(info)
+	ExitOnError(err)
+	header.Name = filepath.Base(filePath)
+
+	writer, err := zipWriter.CreateHeader(header)
+	ExitOnError(err)
+
+	// 复制文件内容到ZIP
+	_, err = io.Copy(writer, file)
+	ExitOnError(err)
+}
