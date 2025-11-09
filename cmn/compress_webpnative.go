2b562f4b8c15d41bd21e58dab5236e4499a19ead
gotoeasy <gotoeasy@163.com>
2025-11-09 14:59:10 +0800

    v4.2.1

--- cmn/compress_webpnative.go
+++ cmn/compress_webpnative.go
@@ -1,28 +1,26 @@
-//go:build !cgo
-
 package cmn
 
-import (
-	"bytes"
-	"image"
+// import (
+// 	"bytes"
+// 	"image"
 
-	"github.com/HugoSmits86/nativewebp"
-)
+// 	"github.com/HugoSmits86/nativewebp"
+// )
 
-func (c *nativeCompressor) Compress(data []byte, width int, height int) ([]byte, error) {
-	img := image.NewNRGBA(image.Rect(0, 0, width, height))
-	copy(img.Pix, data)
+// func (c *nativeCompressor) Compress(data []byte, width int, height int) ([]byte, error) {
+// 	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+// 	copy(img.Pix, data)
 
-	var buf bytes.Buffer
-	err := nativewebp.Encode(&buf, img, nil)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
+// 	var buf bytes.Buffer
+// 	err := nativewebp.Encode(&buf, img, nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return buf.Bytes(), nil
+// }
 
-type nativeCompressor struct{}
+// type nativeCompressor struct{}
 
-func init() {
-	webpComp = &nativeCompressor{}
-}
+// func init() {
+// 	webpComp = &nativeCompressor{}
+// }
--- go.mod
+++ go.mod
@@ -5,8 +5,13 @@ go 1.25.1
 require (
 	github.com/HugoSmits86/nativewebp v1.2.0
 	github.com/chai2010/webp v1.4.0
+	github.com/gen2brain/webp v0.5.5
 	github.com/klauspost/compress v1.18.0
 	github.com/ulikunitz/xz v0.5.12
 )
 
-require golang.org/x/image v0.24.0 // indirect
+require (
+	github.com/ebitengine/purego v0.8.3 // indirect
+	github.com/tetratelabs/wazero v1.9.0 // indirect
+	golang.org/x/image v0.24.0 // indirect
+)
