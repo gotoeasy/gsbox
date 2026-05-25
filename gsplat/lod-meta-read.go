5388869f8309665f65ecd3a6798e7ccff88af20a
gotoeasy <gotoeasy@163.com>
2026-05-25 13:11:43 +0800

    net

--- gsplat/lod-meta-read.go
+++ gsplat/lod-meta-read.go
@@ -4,20 +4,36 @@ import (
 	"encoding/json"
 	"errors"
 	"gsbox/cmn"
+	"log"
 	"path/filepath"
+	"strings"
+	"sync"
 )
 
 func ReadLodMeta(input string) (dataShDegree uint8, lodLevels uint16, lodDatas []*SplatData, envDatas []*SplatData) {
 	cmn.ExitOnConditionError(!cmn.Endwiths(input, "lod-meta.json"), errors.New("invalid lod-meta.json: "+input))
 
-	jsonStr, err := cmn.ReadFileString(input)
+	isNetFile := cmn.IsNetFile(input)
+	tmpdir := ""
+	inputTmp := input
+	if isNetFile {
+		tmpdir = downloadLod(input)
+		inputTmp = filepath.Join(tmpdir, cmn.FileName(input))
+	}
+	defer func() {
+		if isNetFile {
+			cmn.RemoveAllFile(cmn.Dir(tmpdir))
+		}
+	}()
+
+	jsonStr, err := cmn.ReadFileString(inputTmp)
 	cmn.ExitOnError(err)
 
 	var lodMeta LodMeta
 	err = json.Unmarshal([]byte(jsonStr), &lodMeta)
 	cmn.ExitOnError(err)
 
-	lodMetaDir := cmn.Dir(input)
+	lodMetaDir := cmn.Dir(inputTmp)
 
 	// 汇总文件
 	mapFiles := make(map[string]*SplatFile)
@@ -79,4 +95,95 @@ func traveTree(node *LodNode, fnCallBack func(nd *LodNode) bool) {
 			traveTree(cld, fnCallBack)
 		}
 	}
+}
+
+func downloadLod(input string) string {
+	tmpdir, err := cmn.CreateTempDir()
+	cmn.ExitOnError(err)
+	jsonFileName := cmn.FileName(input)
+	localJsonFile := filepath.Join(tmpdir, jsonFileName) // lod json
+	log.Println("[Info]", "download", jsonFileName)
+	err = cmn.HttpDownload(input, localJsonFile, nil)
+	cmn.RemoveAllFileIfError(err, tmpdir)
+	cmn.ExitOnError(err)
+
+	jsonStr, err := cmn.ReadFileString(localJsonFile)
+	cmn.ExitOnError(err)
+
+	var lodMeta LodMeta
+	err = json.Unmarshal([]byte(jsonStr), &lodMeta)
+	cmn.ExitOnError(err)
+
+	ary := strings.Split(input, "/")
+	ary = ary[:len(ary)-1]
+	baseUrl := cmn.Join(ary, "/")
+
+	// 汇总文件
+	var files []string
+	for _, filename := range lodMeta.Filenames {
+		urlFile := baseUrl + "/" + filename
+		localFile := filepath.Join(tmpdir, filename)
+		localDir := cmn.Dir(localFile)
+		log.Println("[Info]", "download", filename)
+		if cmn.Endwiths(filename, "meta.json") {
+			downloadSogOfMetaJson(urlFile, localDir)
+		} else {
+			err := cmn.HttpDownload(urlFile, localFile, nil)
+			cmn.ExitOnError(err)
+		}
+	}
+	if lodMeta.Environment != "" {
+		files = append(files, filepath.Join(baseUrl, baseUrl+"/"+lodMeta.Environment))
+		urlFile := baseUrl + "/" + lodMeta.Environment
+		localFile := filepath.Join(tmpdir, lodMeta.Environment)
+		localDir := cmn.Dir(localFile)
+		log.Println("[Info]", "download", lodMeta.Environment)
+		if cmn.Endwiths(lodMeta.Environment, "meta.json") {
+			downloadSogOfMetaJson(urlFile, localDir)
+		} else {
+			err := cmn.HttpDownload(urlFile, localFile, nil)
+			cmn.ExitOnError(err)
+		}
+	}
+
+	return tmpdir
+}
+
+func downloadSogOfMetaJson(urlMetaJson string, saveDir string) {
+
+	fileMeta := filepath.Join(saveDir, "meta.json")
+	err := cmn.HttpDownload(urlMetaJson, filepath.Join(saveDir, "meta.json"), nil)
+	cmn.ExitOnError(err)
+	strMeta, err := cmn.ReadFileString(fileMeta)
+	cmn.ExitOnError(err)
+
+	meta, err := ParseSogMeta(strMeta)
+	cmn.ExitOnError(err)
+
+	var wg sync.WaitGroup
+	ary := cmn.Split(urlMetaJson, "/")
+	ary[len(ary)-1] = meta.Means.Files[0]
+	wg.Add(1)
+	go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.Means.Files[0]), &wg)
+	ary[len(ary)-1] = meta.Means.Files[1]
+	wg.Add(1)
+	go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.Means.Files[1]), &wg)
+	ary[len(ary)-1] = meta.Scales.Files[0]
+	wg.Add(1)
+	go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.Scales.Files[0]), &wg)
+	ary[len(ary)-1] = meta.Quats.Files[0]
+	wg.Add(1)
+	go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.Quats.Files[0]), &wg)
+	ary[len(ary)-1] = meta.Sh0.Files[0]
+	wg.Add(1)
+	go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.Sh0.Files[0]), &wg)
+	if meta.ShN != nil {
+		ary[len(ary)-1] = meta.ShN.Files[0]
+		wg.Add(1)
+		go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.ShN.Files[0]), &wg)
+		ary[len(ary)-1] = meta.ShN.Files[1]
+		wg.Add(1)
+		go cmn.HttpDownload(cmn.Join(ary, "/"), filepath.Join(saveDir, meta.ShN.Files[1]), &wg)
+	}
+	wg.Wait() // 阻塞直到所有下载完成
 }
